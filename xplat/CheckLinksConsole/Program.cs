086303483371a94afd7188b3c66b1ae6d113f427
degoeym <degoeym@gmail.com>
2017-05-08 16:47:45 -0400

    Through part two

--- xplat/CheckLinksConsole/Program.cs
+++ xplat/CheckLinksConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
 
 namespace CheckLinksConsole
 {
@@ -6,7 +8,12 @@ namespace CheckLinksConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
+            var client = new HttpClient();
+            var body = client.GetStringAsync(site);
+            Console.WriteLine(body.Result);
+            var links = LinkChecker.GetLinks(body.Result);
+            links.ToList().ForEach(Console.WriteLine);
         }
     }
 }
